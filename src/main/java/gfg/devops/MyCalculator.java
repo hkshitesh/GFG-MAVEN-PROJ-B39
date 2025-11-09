9cae883de2d9a38766dbbca1f112474a7a958e83
Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-11-09 17:48:45 +0530

    dev branch

--- src/main/java/gfg/devops/MyCalculator.java
+++ src/main/java/gfg/devops/MyCalculator.java
@@ -22,6 +22,10 @@ public class MyCalculator {
 	{
 		return a%b;
 	}
+	public void show()
+	{
+		System.out.println("Dev Brach");
+	}
 
 	public static void main(String[] args) {
 		
@@ -31,5 +35,6 @@ public class MyCalculator {
 		System.out.println("Mul is :"+calc.mul(20, 10));
 		System.out.println("Div is :"+calc.div(20, 10));
 		System.out.println("Mod is :"+calc.mod(20, 10));
+		calc.show();
 	}
 }
